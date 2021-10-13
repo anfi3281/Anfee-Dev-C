19ec14ece297636af13bbb6cf98fb89c576f9549
anfi3281 <anfi3281@gmail.com>
2021-10-13 09:09:52 +0700

    Belajar baru

--- index.c
+++ index.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
 int main(){
-    printf("Hello World!");
-    printf("\n");
+    int nilai=1092;
     int x;
+    char halo[] = "halo";
+    printf("%s\t%d\n",halo,nilai);
+    printf("%d\n", nilai);
     for ( x = 1; x <=10; x++){
-        printf("halo ",x);
-        printf("\n");
+        printf("%d\n",x);
     }
     return 0;
 }
