f32d1363e97be09953c30efc11753fbeaedaa9c8
anfi3281 <anfi3281@gmail.com>
2021-10-13 07:58:21 +0700

    Mengubah index.c

---
+++ index.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello World!");
+    printf("\n");
+    int x;
+    for ( x = 1; x <=10; x++){
+        printf("halo ",x);
+        printf("\n");
+    }
+    return 0;
+}
