5631f930ca26c1a97f1b4c347bc09a439dfa014d
anfi3281 <anfi3281@gmail.com>
2021-10-14 15:02:22 +0700

    nomor3

--- index.c
+++ index.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main(){
-    
+    int a, b, c, d;
+    a = 1;
+    b = 2;
+    c = 3;
+    d = 4;
+
+    printf("%d\n", a%b);
+    printf("%d\n", a-b);
+    printf("%d\n", a+b);
+    printf("%d\n", a/b);
+    printf("%d\n", a/d*d+a%d);
+    printf("%d\n", a%d/d*a-c);
     return 0;
 }
