ae9dfcaa8817c574948674c58c3558e201447acb
anfi3281 <anfi3281@gmail.com>
2021-10-14 07:43:45 +0700

    mmm

--- index.c
+++ index.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-
+int nim;
 int main(){
     char nama[100], alamat[100], kuliah[100],jurusan[100], kelas[100];
-    int nim;
+    
     scanf("%d", $nim);
-
+    printf("%d", nim);
     return 0;
 }
