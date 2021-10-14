70ce5f941d0da8b81f1d82bed13cdb2a08cec18f
anfi3281 <anfi3281@gmail.com>
2021-10-14 14:30:59 +0700

    Tugas nomor 1

--- index.c
+++ index.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(){
-    
+int main(){    
+    int Var_bulat = 32767;
+    float Var_pecahan1 = 339.2354767f;
+    double Var_pecahan2 = 3.4657e+40;
+    char VAR_KARAKTER[4] = "'5'";
+
+    printf("%d\n", Var_bulat);
+    printf("%f\n", Var_pecahan1);
+    printf("%f\n", Var_pecahan2);
+    printf("%s\n",VAR_KARAKTER);
     return 0;
 }
