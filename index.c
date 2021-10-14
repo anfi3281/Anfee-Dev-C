dea0c71154a7f83b60a560e5852e32c4ebbd24dc
anfi3281 <anfi3281@gmail.com>
2021-10-14 10:26:10 +0700

    Aplikasi Biodata

--- index.c
+++ index.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
-
+#include <string.h>
 
 int main(){
     char nama[100], alamat[100], kuliah[100],jurusan[100], kelas[100];
-    int nilai=0;    
-    scanf("%s", nama);
-    printf("%s%s\n","sdagdgnama kamu adalah\t", nama);
+    int nilai, nim;    
+
+    printf("Nama : ");
+    gets(nama);
+    printf("Alamat : ");
+    gets(alamat);
+    printf("Kuliah : ");
+    gets(kuliah);
+    printf("Jurusan : ");
+    gets(jurusan);
+    printf("Kelas : ");
+    gets(kelas);
+    printf("NIM : ");
+    scanf("%d", &nim);
+
+    printf("\nNama Anda adalah %s\n", nama);
+    printf("Alamat Anda adalah %s\n", alamat);
+    printf("Kuliah di %s\n",kuliah);
+    printf("Jurusan %s\n", jurusan);
+    printf("Kelas %s\n", kelas);
+    printf("NIM Anda adalah %d\n", nim);
+
+    // scanf("%d", &nilai);
+    // printf("%s%d","sdagdgnama kamu adalah\t", nilai);
     return 0;
 }
