cc5a8ec15e57eff8c3cbc98956d99b7ec40843b2
anfi3281 <anfi3281@gmail.com>
2021-10-14 08:03:45 +0700

    menambah

--- index.c
+++ index.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <string.h>
-int nim;
+
+
 int main(){
     char nama[100], alamat[100], kuliah[100],jurusan[100], kelas[100];
-    
-    scanf("%d", $nim);
-    printf("%d", nim);
+    int nilai=0;    
+    scanf("%s", nama);
+    printf("%s%s\n","sdagdgnama kamu adalah\t", nama);
     return 0;
 }
