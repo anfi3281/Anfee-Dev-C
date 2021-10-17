cd7ec506e389ec23572b98e41bb4efe98d656d03
anfi3281 <anfi3281@gmail.com>
2021-10-18 05:25:09 +0700

    nomor4

--- index.c
+++ index.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main(){
+    int a, b, c, D;
     
+    printf("%s", "Nilai a = ");
+    scanf("%d", &a);
+    printf("%s", "Nilai b = ");
+    scanf("%d", &b);
+    printf("%s", "Nilai c = ");
+    scanf("%d", &c);
+
+    D = b-4*a*c;
+
+    printf("%s%d\n", "Nilai Diskriminannya adalah ", D);
     return 0;
 }
