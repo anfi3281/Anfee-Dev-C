8e1b6d980ba2f94948122a44078a03923fafd28d
Randi Afif <91402751+anfi3281@users.noreply.github.com>
2021-10-18 05:15:30 +0700

    Tugas nomor 2

--- index.c
+++ index.c
@@ -4,11 +4,11 @@ int main(){
     int Var_bulat = 32767;
     float Var_pecahan1 = 339.2354767f;
     double Var_pecahan2 = 3.4657e+40;
-    char VAR_KARAKTER[4] = "'5'";
+    char VAR_KARAKTER = '5';
 
     printf("%d\n", Var_bulat);
     printf("%f\n", Var_pecahan1);
     printf("%f\n", Var_pecahan2);
-    printf("%s\n",VAR_KARAKTER);
+    printf("%c\n",VAR_KARAKTER);
     return 0;
 }
