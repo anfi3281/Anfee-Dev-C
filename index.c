afd828e3238c1f86ed036ad411923e9b2dd0f585
anfi3281 <anfi3281@gmail.com>
2021-10-18 05:48:21 +0700

    update nomor 4

--- index.c
+++ index.c
@@ -2,17 +2,18 @@
 
 int main(){
     int a, b, c, D;
-    
-    printf("%s", "Nilai a = ");
+
+    printf("%s", "Nilai a : ");
     scanf("%d", &a);
-    printf("%s", "Nilai b = ");
+    printf("%s", "Nilai b : ");
     scanf("%d", &b);
-    printf("%s", "Nilai c = ");
+    printf("%s", "Nilai c : ");
     scanf("%d", &c);
 
     D = b-4*a*c;
 
 
     printf("%s%d\n", "Nilai Diskriminannya adalah ", D);
+
     return 0;
 }
